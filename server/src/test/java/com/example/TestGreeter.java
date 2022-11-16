1be3517049b8a93dfb14376a36dbc8b4fded9c55
Kenday I <salige06@gmail.com>
2022-11-15 20:11:15 -0500

    testing scan

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -1,33 +1,18 @@
 package com.example;
-
-import org.junit.Before;
-import org.junit.Test;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.junit.Assert.assertThat;
-import static org.junit.matchers.JUnitMatchers.containsString;
-
-public class TestGreeter {
-
-  private Greeter greeter;
-
-  @Before
-  public void setup() {
-    greeter = new Greeter();
+/**
+ * This is a class.
+ */
+public class Greeter {
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
   }
-
-  @Test
-  public void greetShouldIncludeTheOneBeingGreeted() {
-    String someone = "World";
-
-    assertThat(greeter.greet(someone), containsString(someone));
-  }
-
-  @Test
-  public void greetShouldIncludeGreetingPhrase() {
-    String someone = "World";
-
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+ /**
+   * This is a method.
+   */
+  public final String greet(final String someone) {
+    String DB_PASSWORD = "Admin@12345";
+    return String.format("Hello Ken, %s!", someone);
   }
 }
